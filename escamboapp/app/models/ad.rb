979ee0e3e05d5d84643dbfadcefa05b4b0721d8a
Maycon Ribeiro <maycon_ribeiro@hotmail.com>
2018-11-01 21:24:58 -0300

    ads anuncios publish

--- escamboapp/app/models/ad.rb
+++ escamboapp/app/models/ad.rb
@@ -1,4 +1,11 @@
 class Ad < ActiveRecord::Base
+
   belongs_to :category
   belongs_to :member
+
+  monetize :price_cents
+
+  has_attached_file :picture, styles: { medium: "320x150>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  # validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
+
 end
